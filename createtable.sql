7f5c932567bb0384f929f9a0d8b0a92f45231c7e
ivan <ivanc2k3@gmail.com>
2024-06-28 22:07:22 +0800

    Second Upload

--- createtable.sql
+++ createtable.sql
@@ -1,158 +1,158 @@
-CREATE TABLE IF NOT EXISTS outcome_type (
+CREATE TABLE IF NOT EXISTS public.outcome_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(40) NOT NULL DEFAULT 'no data',
     code VARCHAR(15) NOT NULL DEFAULT 1
-);
+)
 
-CREATE TABLE IF NOT EXISTS action_type (
+CREATE TABLE IF NOT EXISTS public.action_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data',
     technicalType VARCHAR(15) NOT NULL DEFAULT 'Zero',
     code VARCHAR(5) NOT NULL DEFAULT 1
-);
+)
 
-CREATE TABLE IF NOT EXISTS point_type (
+CREATE TABLE IF NOT EXISTS public.point_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     code VARCHAR(10) NOT NULL, --rename
     nameEng VARCHAR(40) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS spin_type (
+CREATE TABLE IF NOT EXISTS public.spin_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data',
     code VARCHAR(5) DEFAULT 1
-);
+)
 
-CREATE TABLE IF NOT EXISTS strength_type (
+CREATE TABLE IF NOT EXISTS public.strength_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data',
     code VARCHAR(5) NOT NULL DEFAULT 1
-);
+)
 
-CREATE TABLE IF NOT EXISTS hand_type (
+CREATE TABLE IF NOT EXISTS public.hand_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data',
     code VARCHAR(5) NOT NULL DEFAULT 1
-);
+)
 
-CREATE TABLE IF NOT EXISTS strick_type (
+CREATE TABLE IF NOT EXISTS public.strick_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS position_type (
+CREATE TABLE IF NOT EXISTS public.position_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(40) NOT NULL 
-);
+)
 
-CREATE TABLE IF NOT EXISTS group_type (
+CREATE TABLE IF NOT EXISTS public.group_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL
-);
+)
 
-CREATE TABLE IF NOT EXISTS serve_type (
+CREATE TABLE IF NOT EXISTS public.serve_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(2) NOT NULL
-);
+)
 
-CREATE TABLE IF NOT EXISTS game_level_type (
+CREATE TABLE IF NOT EXISTS public.game_level_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     nameEng VARCHAR(50) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS game_category_type (
+CREATE TABLE IF NOT EXISTS public.game_category_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS game_format_type (
+CREATE TABLE IF NOT EXISTS public.game_format_type (
     id SERIAL PRIMARY KEY, -- auto_increment
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS technical_style_type (
+CREATE TABLE IF NOT EXISTS public.technical_style_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     nameEng VARCHAR(50) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS rubber_type (
+CREATE TABLE IF NOT EXISTS public.rubber_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(20) NOT NULL,
     nameEng VARCHAR(50) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS grip_type (
+CREATE TABLE IF NOT EXISTS public.grip_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(15) NOT NULL,
     nameEng VARCHAR(50) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS recket_hand_type (
+CREATE TABLE IF NOT EXISTS public.recket_hand_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(30) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS nationality_type (
+CREATE TABLE IF NOT EXISTS public.nationality_type (
     id SERIAL PRIMARY KEY,
     code VARCHAR(4) NOT NULL,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS sex_type (
+CREATE TABLE IF NOT EXISTS public.sex_type (
     id SERIAL PRIMARY KEY,
     name VARCHAR(10) NOT NULL,
     nameEng VARCHAR(20) NOT NULL DEFAULT 'no data'
-);
+)
 
-CREATE TABLE IF NOT EXISTS user_registion (
-    tel INT(20) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS public.user_registion (
+    tel INT NOT NULL,
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     password VARCHAR NOT NULL,
     department VARCHAR(50),
-    categoryId INT(2),
+    categoryId INT,
     mail VARCHAR(50) NOT NULL,
-    nationalityId INT(4) UNSIGNED NOT NULL,
-    masterId INT(10) UNSIGNED,
-    userId INT(10) UNSIGNED,
-    plan1 INT(1) UNSIGNED,
-    plan2 INT(1) UNSIGNED,
-    plan3 INT(1) UNSIGNED,
-    plan4 INT(1) UNSIGNED,
+    nationalityId INT NOT NULL,
+    masterId INT,
+    userId INT,
+    plan1 INT,
+    plan2 INT,
+    plan3 INT,
+    plan4 INT,
     FOREIGN KEY (nationalityId)
         REFERENCES nationality_type(id)
-);
+)
 
-CREATE TABLE IF NOT EXISTS player (
+CREATE TABLE IF NOT EXISTS public.player (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    sexId INT(2) UNSIGNED NOT NULL,
+    sexId INT NOT NULL,
     birthday DATE,
-    nationalityId INT(4) UNSIGNED NOT NULL,
-    bestRecord INT(6) UNSIGNED NOT NULL,
-    worldRanking INT(6) UNSIGNED NOT NULL,
-    recketHandId INT(2) UNSIGNED NOT NULL,
-    gripId INT(2) UNSIGNED NOT NULL,
-    rubberId INT(10) UNSIGNED NOT NULL,
-    technicalStyleId INT(2) UNSIGNED NOT NULL,
-    builderId INT(11),
-    userId INT(10) UNSIGNED,
-    ballage INT(2) UNSIGNED,
+    nationalityId INT NOT NULL,
+    bestRecord INT NOT NULL,
+    worldRanking INT NOT NULL,
+    recketHandId INT NOT NULL,
+    gripId INT NOT NULL,
+    rubberId INT NOT NULL,
+    technicalStyleId INT NOT NULL,
+    builderId INT,
+    userId INT,
+    ballage INT,
     FOREIGN KEY (sexId)
         REFERENCES sex_type(id),
     FOREIGN KEY (nationalityId)
@@ -165,20 +165,20 @@ CREATE TABLE IF NOT EXISTS player (
         REFERENCES rubber_type(id),
     FOREIGN KEY (technicalStyleId)
         REFERENCES technical_style_type(id)
-);
+)
 
-CREATE TABLE IF NOT EXISTS game (
+CREATE TABLE IF NOT EXISTS public.game (
     id SERIAL PRIMARY KEY,
     name VARCHAR(45) NOT NULL,
-    formatId INT(2) UNSIGNED NOT NULL,
+    formatId INT NOT NULL,
     date DATE,
-    serveId INT(2) UNSIGNED NOT NULL,
-    categoryId INT(2) UNSIGNED NOT NULL,
-    levelId INT(3) UNSIGNED NOT NULL,
-    builderId1 INT(11),
-    builderId2 INT(11),
+    serveId INT NOT NULL,
+    categoryId INT NOT NULL,
+    levelId INT NOT NULL,
+    builderId1 INT,
+    builderId2 INT,
     notes VARCHAR(4000),
-    groupId INT(2) UNSIGNED NOT NULL DEFAULT 11,
+    groupId INT NOT NULL DEFAULT 11,
     isAnalyzed BOOLEAN DEFAULT 0, --  new
     FOREIGN KEY (formatId)
         REFERENCES game_format_type(id),
@@ -188,22 +188,22 @@ CREATE TABLE IF NOT EXISTS game (
         REFERENCES game_category_type(id),
     FOREIGN KEY (levelId)
         REFERENCES game_level_type(id)
-);
+)
 
-CREATE TABLE IF NOT EXISTS game_player (
+CREATE TABLE IF NOT EXISTS public.game_player (
     id SERIAL PRIMARY KEY,
-    serveId INT(2) UNSIGNED NOT NULL,
-    gameId INT(11) UNSIGNED NOT NULL,
-    playerId INT(10) UNSIGNED NOT NULL,
+    serveId INT NOT NULL,
+    gameId INT NOT NULL,
+    playerId INT NOT NULL,
     name VARCHAR(30) NOT NULL,
-    sexId INT(2) UNSIGNED NOT NULL,
-    nationalityId INT(4) UNSIGNED NOT NULL,
-    worldRanking INT(6) UNSIGNED NOT NULL,
-    recketHandId INT(2) UNSIGNED NOT NULL,
-    gripId INT(2) UNSIGNED NOT NULL,
-    rubberId INT(10) UNSIGNED NOT NULL,
-    technicalStyleId INT(2) UNSIGNED NOT NULL,
-    isWin INT(2) UNSIGNED NOT NULL DEFAULT 0,
+    sexId INT NOT NULL,
+    nationalityId INT NOT NULL,
+    worldRanking INT NOT NULL,
+    recketHandId INT NOT NULL,
+    gripId INT NOT NULL,
+    rubberId INT NOT NULL,
+    technicalStyleId INT NOT NULL,
+    isWin INT NOT NULL DEFAULT 0,
     FOREIGN KEY (serveId)
         REFERENCES serve_type(id),
     FOREIGN KEY (gameId)
@@ -224,14 +224,14 @@ CREATE TABLE IF NOT EXISTS game_player (
         REFERENCES rubber_type(id),
     FOREIGN KEY (technicalStyleId)
         REFERENCES technical_style_type(id)
-);
+)
 
--- CREATE TABLE IF NOT EXISTS goodscore(
+-- CREATE TABLE IF NOT EXISTS public.goodscore(
 --     id INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
---     typeId INT(1) UNSIGNED NOT NULL DEFAULT 0,
+--     typeId INT NOT NULL DEFAULT 0,
 --     gamePlayerItemStart BIGINT(25) UNSIGNED NOT NULL,
---     gameId INT(11) UNSIGNED NOT NULL,
---     numberGame TINYINT(3) UNSIGNED NOT NULL,
+--     gameId INT NOT NULL,
+--     numberGame TINYINT NOT NULL,
 --     score VARCHAR(10) NOT NULL,
 --     FOREIGN KEY (gamePlayerItemStart)
 --         REFERENCES game_player(id),
@@ -240,29 +240,29 @@ CREATE TABLE IF NOT EXISTS game_player (
 --         ON DELETE CASCADE
 -- )
 
-CREATE TABLE IF NOT EXISTS game_player_item (
+CREATE TABLE IF NOT EXISTS public.game_player_item (
     id SERIAL PRIMARY KEY,
-    numberGame TINYINT(3) UNSIGNED NOT NULL,
-    gamePlayerId INT(11) UNSIGNED NOT NULL,
-    gamePlayerOtherId INT(11) UNSIGNED NOT NULL DEFAULT 0,
-    serveId INT(2) UNSIGNED NOT NULL,
-    servePoint INT(2) UNSIGNED NOT NULL,
-    serveNumber INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    numberGameA INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    numberGameB INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    strickNumber INT(4) UNSIGNED NOT NULL,
-    scoreSelf INT(3) UNSIGNED NOT NULL DEFAULT 0,
-    scoreOther INT(3) UNSIGNED NOT NULL DEFAULT 0,
-    strickId INT(2) UNSIGNED NOT NULL,
-    handId INT(4) UNSIGNED NOT NULL,
-    strengthId INT(4) UNSIGNED NOT NULL,
-    spinId INT(4) UNSIGNED NOT NULL,
-    pointId INT(4) UNSIGNED NOT NULL,
-    actionId INT(4) UNSIGNED NOT NULL,
-    outcomeId INT(4) UNSIGNED NOT NULL,
-    let INT(11) UNSIGNED NOT NULL, -- origianl: stopGamePlayerId
-    goodId INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    positionId INT(2) UNSIGNED NOT NULL DEFAULT 0,
+    numberGame INT NOT NULL,
+    gamePlayerId INT NOT NULL,
+    gamePlayerOtherId INT NOT NULL DEFAULT 0,
+    serveId INT NOT NULL,
+    servePoint INT NOT NULL,
+    serveNumber INT NOT NULL DEFAULT 0,
+    numberGameA INT NOT NULL DEFAULT 0,
+    numberGameB INT NOT NULL DEFAULT 0,
+    strickNumber INT NOT NULL,
+    scoreSelf INT NOT NULL DEFAULT 0,
+    scoreOther INT NOT NULL DEFAULT 0,
+    strickId INT NOT NULL,
+    handId INT NOT NULL,
+    strengthId INT NOT NULL,
+    spinId INT NOT NULL,
+    pointId INT NOT NULL,
+    actionId INT NOT NULL,
+    outcomeId INT NOT NULL,
+    let INT NOT NULL, -- origianl: stopGamePlayerId
+    goodId INT NOT NULL DEFAULT 0,
+    positionId INT NOT NULL DEFAULT 0,
     FOREIGN KEY (gamePlayerId)
         REFERENCES game_player(id)
         ON DELETE CASCADE,
@@ -286,30 +286,30 @@ CREATE TABLE IF NOT EXISTS game_player_item (
     --     REFERENCES goodscore(id)
     FOREIGN KEY (positionId)
         REFERENCES position_type(id)
-);
+)
 
-CREATE TABLE IF NOT EXISTS game_player_double_item (
+CREATE TABLE IF NOT EXISTS public.game_player_double_item (
     id SERIAL PRIMARY KEY,
-    numberGame TINYINT(3) UNSIGNED NOT NULL,
-    gamePlayerId INT(11) UNSIGNED NOT NULL,
-    gamePlayerOtherId INT(11) UNSIGNED NOT NULL DEFAULT 0,
-    serveId INT(2) UNSIGNED NOT NULL,
-    servePoint INT(2) UNSIGNED NOT NULL,
-    serveNumber INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    numberGameA INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    numberGameB INT(2) UNSIGNED NOT NULL DEFAULT 0,
-    strickNumber INT(4) UNSIGNED NOT NULL,
-    scoreSelf INT(3) UNSIGNED NOT NULL DEFAULT 0,
-    scoreOther INT(3) UNSIGNED NOT NULL DEFAULT 0,
-    strickId INT(2) UNSIGNED NOT NULL,
-    handId INT(4) UNSIGNED NOT NULL,
-    strengthId INT(4) UNSIGNED NOT NULL,
-    spinId INT(4) UNSIGNED NOT NULL,
-    pointId INT(4) UNSIGNED NOT NULL,
-    actionId INT(4) UNSIGNED NOT NULL,
-    outcomeId INT(4) UNSIGNED NOT NULL,
-    let INT(11) UNSIGNED NOT NULL, -- origianl: stopGamePlayerId
-    goodId INT(2) UNSIGNED NOT NULL DEFAULT 0,
+    numberGame INT NOT NULL,
+    gamePlayerId INT NOT NULL,
+    gamePlayerOtherId INT NOT NULL DEFAULT 0,
+    serveId INT NOT NULL,
+    servePoint INT NOT NULL,
+    serveNumber INT NOT NULL DEFAULT 0,
+    numberGameA INT NOT NULL DEFAULT 0,
+    numberGameB INT NOT NULL DEFAULT 0,
+    strickNumber INT NOT NULL,
+    scoreSelf INT NOT NULL DEFAULT 0,
+    scoreOther INT NOT NULL DEFAULT 0,
+    strickId INT NOT NULL,
+    handId INT NOT NULL,
+    strengthId INT NOT NULL,
+    spinId INT NOT NULL,
+    pointId INT NOT NULL,
+    actionId INT NOT NULL,
+    outcomeId INT NOT NULL,
+    let INT NOT NULL, -- origianl: stopGamePlayerId
+    goodId INT NOT NULL DEFAULT 0,
     FOREIGN KEY (gamePlayerId)
         REFERENCES game_player(id)
         ON DELETE CASCADE,
@@ -329,4 +329,4 @@ CREATE TABLE IF NOT EXISTS game_player_double_item (
         REFERENCES action_type(id),
     FOREIGN KEY (outcomeId)
         REFERENCES outcome_type(id)
-);
+)
